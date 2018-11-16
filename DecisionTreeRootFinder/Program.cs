3750018bf84fdf411d2125834dec711818f10dc1
Oranye Nonso <nonicalx@gmail.com>
2018-11-16 21:40:47 +0100

    Create CSVreader method

--- DecisionTreeRootFinder/Program.cs
+++ DecisionTreeRootFinder/Program.cs
@@ -9,35 +9,42 @@ namespace DecisionTreeRootFinder
         static void Main(string[] args)
         {
             var filePath = @"C:\Users\ORANYE-A.N\Desktop\sample.csv";
-            Console.WriteLine("Hello World!");
-            string[] content1 = new string[10];
-            var atr1 = new List<string>();
-            var reader = new StreamReader(File.OpenRead(filePath));
+            CSVReader(filePath);
 
-            List<string> result = new List<string>();
-
-            while (!reader.EndOfStream)
-            {
-                var line = reader.ReadLine();
-                result.Add(line);
-            }
-            for (int i = 0; i < result.Count; i++)
-            {
-
-                Console.WriteLine(result[i]);
-                string extract = result[i].ToString();
-                content1 = extract.Split(',');
-                Console.WriteLine(content1[0]);
-                atr1.Add(content1[0]);
-                Console.WriteLine();
-            }
-
-            foreach (var item in atr1)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(item);
-            }
-            Console.ReadLine();
         }
+            public static string[] CSVReader(string path)
+            {
+                List<string> result = new List<string>();
+                string[] atributeArray = new string[result.Count];
+                var atribute = new List<string>();
+                var reader = new StreamReader(File.OpenRead(path));
+
+                
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    result.Add(line);
+                }
+                for (int i = 0; i < result.Count; i++)
+                {
+
+                    Console.WriteLine(result[i]);
+                    string extract = result[i].ToString();
+                    atributeArray = extract.Split(',');
+                    Console.WriteLine(atributeArray[0]);
+                    atribute.Add(atributeArray[0]);
+                    Console.WriteLine();
+                }
+
+                foreach (var item in atributeArray)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(item);
+                }
+                Console.ReadLine();
+
+                return;
+            }
     }
 }
