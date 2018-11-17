0e3b39b90ff96b4751744d2729e13453009ad795
Oranye Nonso <nonicalx@gmail.com>
2018-11-17 23:23:02 +0100

    Created AttributeEntropyCalculator method and extracted unique elemets from the atribute list.

--- DecisionTreeRootFinder/Program.cs
+++ DecisionTreeRootFinder/Program.cs
@@ -9,17 +9,16 @@ namespace DecisionTreeRootFinder
         static void Main(string[] args)
         {
             var filePath = @"C:\Users\ORANYE-A.N\Desktop\sample.csv";
-            CSVReader(filePath);
-
+            var output = CSVReader(filePath);
+            AttributeEntropyCalculator(output);
+            Console.ReadLine();
         }
-            public static List<string> CSVReader(string path)
-            {
+        public static List<string> CSVReader(string path)
+        {
                 List<string> result = new List<string>();
                 string[] atributeArray = new string[result.Count];
                 var atribute = new List<string>();
                 var reader = new StreamReader(File.OpenRead(path));
-
-                
 
                 while (!reader.EndOfStream)
                 {
@@ -35,5 +34,20 @@ namespace DecisionTreeRootFinder
 
                 return atribute;
             }
+            
+        public static  double AttributeEntropyCalculator(List<string> atribute)
+        {
+            var uniqueElements = new List<string>();
+            var eachAtribute = new HashSet<string>(atribute);
+            foreach (var item in eachAtribute)
+            {
+                uniqueElements.Add(item);
+                Console.WriteLine(item);
+            }
+
+           double entropy = 0;
+            
+           return entropy;
+        }
     }
 }
