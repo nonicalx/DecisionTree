98c01e8f0eb32aba290bf492772e1dbb1be02141
Oranye Nonso <nonicalx@gmail.com>
2018-11-18 23:04:30 +0100

    Create DataSetEntropy method and ReadCSVFile Method

--- DecisionTreeRootFinder/Program.cs
+++ DecisionTreeRootFinder/Program.cs
@@ -6,44 +6,55 @@ namespace DecisionTreeRootFinder
 {
     class Program
     {
+        static List<string> result = new List<string>();
+        static string filePath = @"C:\Users\ORANYE-A.N\Desktop\sample.csv";
+
         static void Main(string[] args)
         {
-            var filePath = @"C:\Users\ORANYE-A.N\Desktop\sample.csv";
-            var output = CSVReader(filePath);
+            ReadCSVFile();
+            var output = GetAtribute(0);
             AttributeEntropyCalculator(output);
+            DataSetEntropy();
             Console.ReadLine();
         }
-        public static List<string> CSVReader(string path)
+        public static void ReadCSVFile()
         {
-                List<string> result = new List<string>();
-                string[] atributeArray = new string[result.Count];
-                var atribute = new List<string>();
-                var reader = new StreamReader(File.OpenRead(path));
+            var reader = new StreamReader(File.OpenRead(filePath));
 
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    result.Add(line);
-                }
-                for (int i = 0; i < result.Count; i++)
-                {
-                    string extract = result[i].ToString();
-                    atributeArray = extract.Split(',');
-                    atribute.Add(atributeArray[0]);
-                }
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                result.Add(line);
+            }
+        }
 
-                return atribute;
+        public static List<string> GetAtribute(int atributeIndex)
+        {
+            string[] atributeArray = new string[result.Count];
+            Console.WriteLine(result.Count);
+            var atribute = new List<string>();
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                string extract = result[i].ToString();
+                atributeArray = extract.Split(',');
+                atribute.Add(atributeArray[atributeIndex]);
+            }
+
+            return atribute;
             }
             
         public static  double AttributeEntropyCalculator(List<string> atribute)
         {
             var uniqueElements = new List<string>();
             var eachAtribute = new HashSet<string>(atribute);
+
             foreach (var item in eachAtribute)
             {
                 uniqueElements.Add(item);
                 Console.WriteLine(item);
             }
+
             for (int i = 1; i < uniqueElements.Count; i++)
             {
                 int occurance = 0;
@@ -59,11 +70,26 @@ namespace DecisionTreeRootFinder
                     occurance += 1;
                     index = atributeToString.IndexOf(keyword, index + 1);
                 }
-                Console.WriteLine($"{keyword} occurs {occurance}");
+                Console.WriteLine($"{keyword} occurs {occurance} times");
             }
-           double entropy = 0;
+
+           double subsetEntropy = 0;
             
-           return entropy;
+           return subsetEntropy;
+        }
+
+        public static double DataSetEntropy()
+        {
+            var outputList = GetAtribute(4);
+            double dataSetEntroy = 0;
+             var uniqueElements = new List<string>();
+            var eachAtribute = new HashSet<string>(outputList);
+            foreach (var item in eachAtribute)
+            {
+                uniqueElements.Add(item);
+                Console.WriteLine(item);
+            }
+            return dataSetEntroy;
         }
     }
 }
